2d8547dfaec2e0acbb7431f9e1aedeaf9dc8391a
hanbonghun <bng4535@khu.ac.kr>
2023-03-14 11:45:55 +0900

    인스턴스 변수 출력(서버가 켜진동안 값이 유지된다)

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -6,11 +6,26 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller // 개발자가 spring boot에게 말한다.
 public class HomeController {
+    private int increase=0;
     // /home/main 요청이 오면 아래 메서드를 실행해줘
     @GetMapping("/home/main")
     //아래 메서드를 실행 후 그 리턴값을 응답으로 삼아
     @ResponseBody
     public String showMain(){
         return "반갑!";
+    }
+
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showMain2(){return "반가웠습니다.";}
+    @GetMapping("/home/main3")
+    @ResponseBody
+    public String showMain3(){return "즐거웠습니다.";}
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public String showIncrease(){
+        increase++;
+        return "응답 : " + increase;
     }
 }

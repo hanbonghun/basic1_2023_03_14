de60ae9a151b7a7e754eb9a5a478e24075b1576f
hanbonghun <bng4535@khu.ac.kr>
2023-03-16 14:27:19 +0900

    디렉토리 수정, Rq생명주기 변경

--- src/main/java/com/ll/basic1/base/rq/Rq.java
+++ src/main/java/com/ll/basic1/base/rq/Rq.java
@@ -4,9 +4,13 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.Arrays;
 
+@Component
+@RequestScope // 이 객체는 매 요청마다 생성된다.
 @AllArgsConstructor
 public class Rq {
     private final HttpServletRequest req;
@@ -14,23 +18,24 @@ public class Rq {
 
     public boolean removeCookie(String name) {
         if (req.getCookies() != null) {
-            Arrays.stream(req.getCookies())
-                    .filter(cookie -> cookie.getName().equals(name))
-                    .forEach(cookie -> {
-                        cookie.setMaxAge(0);
-                        resp.addCookie(cookie);
-                    });
+            Cookie cookie = Arrays.stream(req.getCookies())
+                    .filter(c -> c.getName().equals(name))
+                    .findFirst()
+                    .orElse(null);
 
-            return Arrays.stream(req.getCookies())
-                    .filter(cookie -> cookie.getName().equals(name))
-                    .count() > 0;
+            if (cookie != null) {
+                cookie.setMaxAge(0);
+                resp.addCookie(cookie);
+
+                return true;
+            }
         }
 
         return false;
     }
 
     public String getCookie(String name, String defaultValue) {
-        if ( req.getCookies() == null ) return defaultValue;
+        if (req.getCookies() == null) return defaultValue;
 
         return Arrays.stream(req.getCookies())
                 .filter(cookie -> cookie.getName().equals(name))
@@ -42,14 +47,13 @@ public class Rq {
     public long getCookieAsLong(String name, long defaultValue) {
         String value = getCookie(name, null);
 
-        if ( value == null ) {
+        if (value == null) {
             return defaultValue;
         }
 
         try {
             return Long.parseLong(value);
-        }
-        catch ( NumberFormatException e ) {
+        } catch (NumberFormatException e) {
             return defaultValue;
         }
     }

88ae5b8c79fae9441fb3efa45e2f875742f30c84
hanbonghun <bng4535@khu.ac.kr>
2023-03-17 11:54:52 +0900

    JPA로 insert

---
+++ src/main/java/com/ll/basic1/boundedContext/article/entity/Article.java
@@ -0,0 +1,24 @@
+package com.ll.basic1.boundedContext.article.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Article {
+    @Id // PRIMARY KEY
+    @GeneratedValue(strategy = IDENTITY) //AUTO_INCREMENT
+    private long id;
+    private LocalDateTime createDate; //데이터 생성 날짜
+    private  LocalDateTime modifyDate; // 데이터 수정 날짜
+    private String title;
+    private String body;
+}
